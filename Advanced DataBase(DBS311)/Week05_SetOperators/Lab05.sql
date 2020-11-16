9ea8c3e35f082fdd4b3d90f5fff26367703e51fd
Dongchan Oh <60357173+chanlenium@users.noreply.github.com>
2020-11-16 12:45:30 -0500

    Add files via upload

---
+++ Advanced DataBase(DBS311)/Week05_SetOperators/Lab05.sql
@@ -0,0 +1,60 @@
+/* QUESTION 1
+List all the countries that start with a letter entered by the user (prompt). 
+The user must enter a lowercase letter g */
+
+
+/* QUESTION 2
+let's make the previous question easier for the user !!!
+
+SELECT * FROM countries
+    WHERE UPPER(country_name) LIKE UPPER('&EnterLetter%');
+
+Try it with UPPER and LOWER to see the effect compared to previous question.
+JUST ANSWER YES if you tried it. */
+
+
+
+/* QUESTION 3
+Display cities that no warehouse is located in them. (use set operators to answer this question) */
+
+
+
+
+/* QUESTION 4
+Again using SET
+Display the category ID, category name, and the number of products in category 1, 2, and 5. 
+In your result, display first the number of products in category 5, then category 1 and then 2. Similar to notes. */
+
+
+
+/* QUESTION 5
+DO NOT ANSWER THIS. It is the same question as #9. Answer it in #9.
+Display product ID for ordered products whose quantity in the inventory is greater than 5. 
+(You are not allowed to use JOIN for this question.) */
+
+
+
+/* QUESTION 6
+Show the date 10 days before today (today is not hard coded but is supplied by the system). 
+It should be obvious this is not about SET operators. */
+
+
+
+/* QUESTION 7
+List all the countries and replace all letter "a"'s with "o"'s. */
+
+
+
+
+/* QUESTION 8
+Use SET operators.   
+We need a single report to display all warehouses and the state that they are located in and all states regardless of whether they have warehouses in them or not.*/
+
+
+
+/* QUESTION 9
+Display product ID for ordered products whose quantity in the inventory is greater than 5. 
+(You are not allowed to use JOIN for this question. Use SET operators) */
+
+
+
