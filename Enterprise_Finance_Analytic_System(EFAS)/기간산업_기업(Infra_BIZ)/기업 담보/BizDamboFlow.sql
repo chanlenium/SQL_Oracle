aaf1d8879522a1b6568128a5d3cf8a47ff4541f2
Dongchan Oh <dynamic.oh@gmail.com>
2022-09-22 17:33:01 +0900

    Add files via upload

--- Enterprise_Finance_Analytic_System(EFAS)/기간산업_기업(Infra_BIZ)/기업 담보/BizDamboFlow.sql
+++ Enterprise_Finance_Analytic_System(EFAS)/기간산업_기업(Infra_BIZ)/기업 담보/BizDamboFlow.sql
@@ -3,19 +3,22 @@
  * 활용 테이블 : BASIC_BIZ_DAMBO, TCB_NICE_FNST -> 기업 담보유동성 기반 테이블(BASIC_BIZ_DAMBO_FLOW) 생성
  **************************************/
 -- 과거 데이터가 있으면 table 삭제하고 새로 생성
-DROP TABLE IF EXISTS BASIC_BIZ_DAMBO_FLOW;
--- 테이블 생성 
+DROP TABLE IF EXISTS BASIC_BIZ_DAMBO_FLOW_TB;
+-- (Standby) 테이블 생성 (기준년월, 법인번호, 기업규모, 담보가액, EFAS, 고객번호, 총자산, 담보가액비율)
 SELECT 
   	t10.*, 
   	t20.COMP_CD, 
-  	t20.TOT_ASSET   	INTO BASIC_BIZ_DAMBO_FLOW 
+  	t20.TOT_ASSET,
+  	t10.BIZ_DAMBO_AMT / NULLIF(t20.TOT_ASSET, 0) as "targetRatio"
+  	INTO BASIC_BIZ_DAMBO_FLOW_TB 
 FROM 
   	(
     -- 연도별, 기업별 담보가액 합 테이블 생성
     SELECT DISTINCT 
      	t.GG_YM, 
        	t.CORP_NO, 
-       	SUM(t.BRNO_DAMBO_AMT) OVER(PARTITION BY t.GG_YM, t.CORP_NO) AS BIZ_DAMBO_AMT,
+       	t.BIZ_SIZE,
+       	SUM(t.BRNO_DAMBO_AMT) OVER(PARTITION BY t.GG_YM, t.CORP_NO, t.BIZ_SIZE) AS BIZ_DAMBO_AMT,
        	t.EFAS
     FROM 
        	BASIC_BIZ_DAMBO t 
@@ -26,6 +29,8 @@ FROM
     		CAST(CONCAT( '${inputYYYY}', '12') as integer) - 200,
     		CAST(CONCAT( '${inputYYYY}', '12') as integer) - 300)
        	AND t.DAMBO_TYPE = '6' -- 담보유형(총계)
+       	AND t.BRWR_NO_TP_CD = '3'	-- 법인만
+		AND NVL(t.EFAS, '') <> '55'	-- 금융보험업 제외
     ORDER BY 
       	t.CORP_NO, t.GG_YM
   	) t10, 
@@ -45,7 +50,7 @@ FROM
               	t.STD_YM, 
               	t.COMP_CD, 
               	t.STD_DT, 
-              	t.AMT as TOT_ASSET, 
+              	ROUND(t.AMT/1000, 0) as TOT_ASSET, -- 단위 변환(천원 -> 백만원)
               	-- 동일 기준일자에 등록된 재무제표가 있으면 기준년월이 최근인 데이터 사용 
               	CAST(MAX(TO_NUMBER(t.STD_YM, '999999')) OVER(PARTITION BY t.COMP_CD, t.STD_DT) AS VARCHAR) as LAST_STD_YM 
             FROM 
@@ -53,11 +58,12 @@ FROM
             WHERE 
               	t.REPORT_CD = '11' -- 제무상태표 테이블
               	AND t.ITEM_CD = '5000' -- 총자산 컬럼
-                AND CAST(t.STD_DT AS INTEGER)  IN (
-                	CAST(CONCAT('${inputYYYY}', '1231') as INTEGER),
-                	CAST(CONCAT('${inputYYYY}', '1231') as INTEGER) - 10000,
-                	CAST(CONCAT('${inputYYYY}', '1231') as INTEGER) - 20000,
-                	CAST(CONCAT('${inputYYYY}', '1231') as INTEGER) - 30000)
+              	AND t.SEAC_DIVN = 'K'	-- 결산 컬럼
+                AND CAST(SUBSTR(t.STD_DT, 1, 4) AS INTEGER)  IN (
+                	CAST('${inputYYYY}' as INTEGER),
+                	CAST('${inputYYYY}' as INTEGER) - 1,
+                	CAST('${inputYYYY}' as INTEGER) - 2,
+                	CAST('${inputYYYY}' as INTEGER) - 3)
           	) t0 
         WHERE 
           	t0.STD_YM = t0.LAST_STD_YM -- 동일 기준일자에 등록된 재무제표가 있으면 기준년월이 최근인 데이터 사용
@@ -71,50 +77,187 @@ WHERE
   	t10.CORP_NO = t20.CORP_NO 
   	AND t10.GG_YM = t20.STD_YM;
 -- 결과 테이블 조회
-SELECT t.* FROM BASIC_BIZ_DAMBO_FLOW t;
- 
- 
- 
+SELECT t.* FROM BASIC_BIZ_DAMBO_FLOW_TB t;
+
 
 
 /********************************************************************
- * 기업 담보 유동성 - 자산 대비 담보가액 추이 (p.1)
- * 자산 대비 담보설정가액 연도별 그래프
+ * 기업 담보 유동성 - 자산 대비 담보가액 추이 (보고서 p.1)
+ * RFP p.12 [그림1] 자산 대비 담보가액 추이
+ * 기업규모별 자산 대비 담보설정가액 연도별 그래프
  *******************************************************************/
-SELECT DISTINCT 
-	t.GG_YM, 
-	-- 연도별 담보설정가액
-  	SUM(t.BIZ_DAMBO_AMT) OVER(PARTITION BY t.GG_YM) as DAMBO_YY, 
-  	-- 연도별 총자산
-  	ROUND(SUM(t.TOT_ASSET / 1000) OVER(PARTITION BY t.GG_YM), 0) as TOT_ASSET_YY, 
-  	-- 자산 대비 담보설정가액 비율(Ratio)
-  	ROUND((SUM(t.BIZ_DAMBO_AMT) OVER(PARTITION BY t.GG_YM)) / (SUM(t.TOT_ASSET / 1000) OVER(PARTITION BY t.GG_YM)), 4) as DAMBO_TO_ASSET_RATIO 
+-- (Step1) Calculate IQR cutoff
+-- 과거 데이터가 있으면 table 삭제하고 새로 생성
+DROP TABLE IF EXISTS IQRcutOff_TB;
+-- 기준년월, LQ(Q1), UQ(Q3), IQR(=UQ-LQ), LowerCutoff, UpperCutoff
+SELECT 
+	t0.*,
+	t0.LQ - 1.5 * (t0.UQ - t0.LQ) as "LowerCutoff",
+	t0.UQ + 1.5 * (t0.UQ - t0.LQ) as "UpperCutoff"
+	INTO IQRcutOff_TB
 FROM 
-  	BASIC_BIZ_DAMBO_FLOW t 
-ORDER BY 
-  	t.GG_YM;
+	(
+	SELECT DISTINCT
+		t.GG_YM,
+		t.BIZ_SIZE,
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY targetRatio) OVER (PARTITION BY t.GG_YM, t.BIZ_SIZE) as "LQ",
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY targetRatio) OVER (PARTITION BY t.GG_YM, t.BIZ_SIZE) as "UQ"	
+	FROM 	
+		BASIC_BIZ_DAMBO_FLOW_TB t
+	) t0;
+-- 결과 조회
+SELECT * FROM IQRcutOff_TB ORDER BY GG_YM, BIZ_SIZE;
+
+
+
+-- (Step2) cutoff 테이블 생성
+-- 과거 데이터가 있으면 table 삭제하고 새로 생성
+DROP TABLE IF EXISTS Cutoff_BASIC_BIZ_DAMBO_FLOW_TB;
+-- 테이블 생성
+SELECT 
+	t1.*
+	INTO Cutoff_BASIC_BIZ_DAMBO_FLOW_TB
+FROM
+	BASIC_BIZ_DAMBO_FLOW_TB t1, IQRcutOff_TB t2
+WHERE 
+	t1.GG_YM = t2.GG_YM
+	AND	t1.BIZ_SIZE = t2.BIZ_SIZE
+	AND t1.targetRatio > t2.LowerCutoff
+	AND t1.targetRatio < t2.UpperCutoff;
+-- 결과 조회
+SELECT * FROM Cutoff_BASIC_BIZ_DAMBO_FLOW_TB;
+
+
+
+-- (Step3) 기업규모별 자산 대비 담보설정가액 비율
+DROP TABLE IF EXISTS RESULT_DAMBO_TO_ASSET_RATIO;
+SELECT
+	t0.GG_YM,
+	t0.TOT_DAMBO_TO_ASSET_RATIO,
+	SUM(DECODE(t0.BIZ_SIZE, '1', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_BIZ_1",	-- 대기업
+	SUM(DECODE(t0.BIZ_SIZE, '2', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_BIZ_2",	-- 중소기업
+	SUM(DECODE(t0.BIZ_SIZE, '3', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_BIZ_3"	-- 중견기업
+	INTO RESULT_DAMBO_TO_ASSET_RATIO
+FROM 	
+	(
+	SELECT DISTINCT 
+		t.GG_YM, 
+		t.BIZ_SIZE,
+		-- 기업규모별 자산 대비 담보설정가액 비율(Ratio)
+		ROUND(AVG(targetRatio) OVER(PARTITION BY t.GG_YM, t.BIZ_SIZE), 4) as "DAMBO_TO_ASSET_RATIO",
+		-- 전기업 자산 대비 담보설정가액 비율(Ratio)
+		ROUND(AVG(targetRatio) OVER(PARTITION BY t.GG_YM), 4) as "TOT_DAMBO_TO_ASSET_RATIO"
+	FROM 
+	  	Cutoff_BASIC_BIZ_DAMBO_FLOW_TB t 
+	ORDER BY 
+	  	t.GG_YM
+	) t0
+WHERE 
+	t0.BIZ_SIZE in ('1', '2', '3')
+GROUP BY
+	t0.GG_YM, t0.TOT_DAMBO_TO_ASSET_RATIO
+ORDER BY
+	t0.GG_YM;
  
  
  
  
- 
+  
+  
+  
+  
+  
+   
 /********************************************************************
- * 기업 담보 유동성 - 산업별 자산 대비 담보가액 (p.1)
+ * 기업 담보 유동성 - 업종별 자산 대비 담보가액 (p.1)
+ * RFP p.12 [그림2] 업종별 자산 대비 담보가액
  * 주요 산업 최근 3개년 그래프
  * (자동차: 37, 조선: 39, 철강: 22, 정유: 11, 석유화학: 12, 반도체: 26, 디스플레이: 27, 해운: 48, 건설: 45)
  *******************************************************************/
-SELECT DISTINCT 
-	TO_NUMBER(t.EFAS, '99'), 
-  	t.GG_YM, 
-  	-- 연도별 담보설정가액
-  	SUM(t.BIZ_DAMBO_AMT) OVER(PARTITION BY t.GG_YM, t.EFAS) as EFAS_DAMBO_YY, 
-  	 -- 연도별 총자산
-  	ROUND(SUM(t.TOT_ASSET / 1000) OVER(PARTITION BY t.GG_YM, t.EFAS), 0) as EFAS_TOT_ASSET_YY, 
-   -- 산업별 자산 대비 담보설정가액 비율(Ratio)
-  	ROUND((SUM(t.BIZ_DAMBO_AMT) OVER(PARTITION BY t.GG_YM, t.EFAS)) / (SUM(t.TOT_ASSET / 1000) OVER(PARTITION BY t.GG_YM, t.EFAS)), 2) as DAMBO_TO_ASSET_RATIO 
+-- (Step1) Calculate IQR cutoff
+-- 과거 데이터가 있으면 table 삭제하고 새로 생성
+DROP TABLE IF EXISTS IQRcutOff_TB;
+-- 기준년월, LQ(Q1), UQ(Q3), IQR(=UQ-LQ), LowerCutoff, UpperCutoff
+SELECT 
+	t0.*,
+	t0.LQ - 1.5 * (t0.UQ - t0.LQ) as "LowerCutoff",
+	t0.UQ + 1.5 * (t0.UQ - t0.LQ) as "UpperCutoff"
+	INTO IQRcutOff_TB
 FROM 
-  	BASIC_BIZ_DAMBO_FLOW t
-WHERE t.EFAS in ('37', '39', '22', '11', '12', '26', '27', '48', '45')
-ORDER BY 
-  	TO_NUMBER(t.EFAS, '99'), 
-  	t.GG_YM;
+	(
+	SELECT DISTINCT
+		t.GG_YM,
+		t.EFAS,
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY targetRatio) OVER (PARTITION BY t.GG_YM, t.EFAS) as "LQ",
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY targetRatio) OVER (PARTITION BY t.GG_YM, t.EFAS) as "UQ"	
+	FROM 	
+		BASIC_BIZ_DAMBO_FLOW_TB t
+	) t0;
+-- 결과 조회
+SELECT * FROM IQRcutOff_TB ORDER BY GG_YM, EFAS;
+
+
+
+-- (Step2) cutoff 테이블 생성
+-- 과거 데이터가 있으면 table 삭제하고 새로 생성
+DROP TABLE IF EXISTS Cutoff_BASIC_BIZ_DAMBO_FLOW_TB;
+-- 테이블 생성
+SELECT 
+	t1.*
+	INTO Cutoff_BASIC_BIZ_DAMBO_FLOW_TB
+FROM
+	BASIC_BIZ_DAMBO_FLOW_TB t1, IQRcutOff_TB t2
+WHERE 
+	t1.GG_YM = t2.GG_YM
+	AND	t1.EFAS = t2.EFAS
+	AND t1.targetRatio > t2.LowerCutoff
+	AND t1.targetRatio < t2.UpperCutoff;
+-- 결과 조회
+SELECT * FROM Cutoff_BASIC_BIZ_DAMBO_FLOW_TB;
+
+
+
+-- (Step3) 주요산업별 자산 대비 담보설정가액 비율
+DROP TABLE IF EXISTS RESULT_DAMBO_TO_ASSET_RATIO_EFAS;
+SELECT
+	t0.GG_YM,
+	SUM(DECODE(t0.EFAS, '11', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS11",	-- 정유
+	SUM(DECODE(t0.EFAS, '12', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS12",	-- 석유화학
+	SUM(DECODE(t0.EFAS, '22', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS22",	-- 철강
+	SUM(DECODE(t0.EFAS, '26', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS26",	-- 반도체
+	SUM(DECODE(t0.EFAS, '27', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS27",	-- 디스플레이
+	SUM(DECODE(t0.EFAS, '37', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS37",	-- 자동차
+	SUM(DECODE(t0.EFAS, '39', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS39",	-- 조선
+	SUM(DECODE(t0.EFAS, '45', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS45",	-- 건설
+	SUM(DECODE(t0.EFAS, '48', t0.DAMBO_TO_ASSET_RATIO, 0)) as "DAMBO_TO_ASSET_RATIO_EFAS48"		-- 해운
+	INTO RESULT_DAMBO_TO_ASSET_RATIO_EFAS
+FROM 	
+	(
+	SELECT DISTINCT 
+		t.GG_YM, 
+		t.EFAS,
+		-- 주요산업별 자산 대비 담보설정가액 비율(Ratio)
+		ROUND(AVG(targetRatio) OVER(PARTITION BY t.GG_YM, t.EFAS), 4) as "DAMBO_TO_ASSET_RATIO"
+	FROM 
+	  	Cutoff_BASIC_BIZ_DAMBO_FLOW_TB t 
+	ORDER BY 
+	  	t.GG_YM
+	) t0
+WHERE 
+	t0.EFAS in ('37', '39', '22', '11', '12', '26', '27', '48', '45')
+GROUP BY
+	t0.GG_YM
+ORDER BY
+	t0.GG_YM;
+	
+
+
+-- 임시테이블 삭제
+DROP TABLE IF EXISTS BASIC_BIZ_DAMBO_FLOW_TB;
+DROP TABLE IF EXISTS IQRcutOff_TB;
+DROP TABLE IF EXISTS Cutoff_BASIC_BIZ_DAMBO_FLOW_TB;
+
+
+-- 결과 조회
+SELECT * FROM RESULT_DAMBO_TO_ASSET_RATIO ORDER BY GG_YM;
+SELECT * FROM RESULT_DAMBO_TO_ASSET_RATIO_EFAS ORDER BY GG_YM;
--- Enterprise_Finance_Analytic_System(EFAS)/기간산업_기업(Infra_BIZ)/기업 담보/BizDamboTrend.sql
+++ Enterprise_Finance_Analytic_System(EFAS)/기간산업_기업(Infra_BIZ)/기업 담보/BizDamboTrend.sql
@@ -1,22 +1,21 @@
 /*****************************************************
- * 담보가액 현황 - 담보가액 추이 (p.1, [그림 1]) 
+ * 담보가액 현황 - 담보가액 추이 (보고서 p.1, [그림 1]) 
+ * RFP p.12 [그림1] 담보가액 추이 
  * 활용 테이블: BASIC_BIZ_DAMBO       
  * 사용자 입력 : 조회기준년월(inputGG_YM)        
  *****************************************************/
+DROP TABLE IF EXISTS RESULT_BIZDamboTrend;
 SELECT DISTINCT 
 	t.GG_YM, 
 	t.DAMBO_TYPE,	  	 -- 담보유형(1:재산권, 2:보증, 5:기타, 6:총계) 
-	CASE 
-  		WHEN t.BIZ_SIZE = 1 THEN t.BIZ_SIZE || ' 대기업'
-  		WHEN t.BIZ_SIZE = 2 THEN t.BIZ_SIZE || ' 중소기업'
-  		WHEN t.BIZ_SIZE = 3 THEN t.BIZ_SIZE || ' 중견기업'
-  		ELSE t.BIZ_SIZE
-  	END as BIZ_SIZE,
-  	round(SUM(t.BRNO_DAMBO_AMT) OVER(PARTITiON BY t.GG_YM, t.DAMBO_TYPE, t.BIZ_SIZE), 0) AS DAMBO_AMT -- 담보유형 및 기준년월별 담보가액 sum
+  	round(SUM(t.BRNO_DAMBO_AMT) OVER(PARTITiON BY t.GG_YM, t.DAMBO_TYPE), 0) AS DAMBO_AMT -- 담보유형 및 기준년월별 담보가액 sum
+  	INTO RESULT_BIZDamboTrend
 FROM 
   	BASIC_BIZ_DAMBO t
 WHERE
-	CAST(t.GG_YM AS INTEGER) <= :inputGG_YM
+	CAST(t.GG_YM AS INTEGER) <= ${inputGG_YM}
+	AND t.BRWR_NO_TP_CD = '3'	-- 법인만
+	AND NVL(t.EFAS, '') <> '55'	-- 금융보험업 제외
 ORDER BY 
 	t.GG_YM;
 
@@ -26,23 +25,30 @@ ORDER BY
   	
 /*****************************************************
  * 담보가액 현황 - 업권별(은행/비은행) 전년 동월 대비 담보가액 변화 (p.1, [그림 1])
+ * RFP p.12 [그림2] 금융업권별 담보가액 
  * 활용 테이블: BASIC_BIZ_DAMBO
  *****************************************************/
+DROP TABLE IF EXISTS RESULT_BIZDamboTrend_UPKWON;
 SELECT 
   	t000.isBANK, 
   	t000.DAMBO_TYPE, 
-  	round(t000.BANK_thisYY + t000.nonBANK_thisYY, 0) as thisYY_AMT, -- 현월 담보가액
-  	round(t000.BANK_prevYY + t000.nonBANK_prevYY, 0) as prevYY_AM -- 전년 동월 담보가액
+  	round(t000.BANK_N1 + t000.nonBANK_N1, 0) as AMT_N2,  -- 전전년 동월 담보가액
+  	round(t000.BANK_N1 + t000.nonBANK_N1, 0) as AMT_N1, -- 전년 동월 담보가액
+  	round(t000.BANK_N0 + t000.nonBANK_N0, 0) as AMT_N0  -- 현월 담보가액
+  	INTO RESULT_BIZDamboTrend_UPKWON
 FROM 
   	(
     SELECT DISTINCT 
     	t00.isBANK, 
-      	t00.DAMBO_TYPE, -- 은행 담보현황(현월, 전년동월)
-      	SUM(CASE WHEN t00.isBANK = '은행' AND t00.GG_YM = :inputGG_YM THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as BANK_thisYY, 
-      	SUM(CASE WHEN t00.isBANK = '은행' AND t00.GG_YM = :inputGG_YM - 100 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as BANK_prevYY, 
+      	t00.DAMBO_TYPE, 
+      	-- 은행 담보현황(현월, 전년동월)
+      	SUM(CASE WHEN t00.isBANK = '은행' AND t00.GG_YM = ${inputGG_YM} THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as BANK_N0, 
+      	SUM(CASE WHEN t00.isBANK = '은행' AND t00.GG_YM = ${inputGG_YM} - 100 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as BANK_N1, 
+      	SUM(CASE WHEN t00.isBANK = '은행' AND t00.GG_YM = ${inputGG_YM} - 200 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as BANK_N2,
       	-- 비은행 담보현황(현월, 전년동월)
-      	SUM(CASE WHEN t00.isBANK = '비은행' AND t00.GG_YM = :inputGG_YM THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as nonBANK_thisYY, 
-      	SUM(CASE WHEN t00.isBANK = '비은행' AND t00.GG_YM = :inputGG_YM - 100 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as nonBANK_prevYY 
+      	SUM(CASE WHEN t00.isBANK = '비은행' AND t00.GG_YM = ${inputGG_YM} THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as nonBANK_N0, 
+      	SUM(CASE WHEN t00.isBANK = '비은행' AND t00.GG_YM = ${inputGG_YM} - 100 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as nonBANK_N1,
+      	SUM(CASE WHEN t00.isBANK = '비은행' AND t00.GG_YM = ${inputGG_YM} - 200 THEN t00.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t00.DAMBO_TYPE, t00.isBANK) as nonBANK_N2
     FROM 
       	(
         SELECT DISTINCT 
@@ -59,7 +65,9 @@ FROM
             FROM 
               	BASIC_BIZ_DAMBO t 
             WHERE 
-            	CAST(t.GG_YM AS INTEGER) in (:inputGG_YM, :inputGG_YM - 100)	-- 당월 및 전년 동월 
+            	CAST(t.GG_YM AS INTEGER) in (${inputGG_YM}, ${inputGG_YM} - 100, ${inputGG_YM} - 200)	-- 최근 3개년 
+            	AND t.BRWR_NO_TP_CD = '3'	-- 법인만
+				AND NVL(t.EFAS, '') <> '55'	-- 금융보험업 제외
           	) t0 
       	) t00
   	) t000;
@@ -71,48 +79,62 @@ FROM
 /*****************************************************
  * 담보가액 현황 - 주요산업별 담보가액
  * 주요 산업 전년 동월 대비 재산권/보증/기타 변화 (p.1, [그림 1])
+ * RFP p.12 [그림2] 업종별 담보가액 
  * (자동차: 37, 조선: 39, 철강: 22, 정유: 11, 석유화학: 12, 반도체: 26, 디스플레이: 27, 해운: 48, 건설: 45)
  * 활용 테이블: BASIC_BIZ_DAMBO
  *****************************************************/
+DROP TABLE IF EXISTS RESULT_BIZDamboTrend_EFAS;
 SELECT 
   	t00.EFAS, 
-  	t00.DAMBO_TYPE, 
-  	-- 현월 담보가액
-  	t00.thisYY_EFAS37 + t00.thisYY_EFAS39 + t00.thisYY_EFAS22 + t00.thisYY_EFAS11 + t00.thisYY_EFAS12 + t00.thisYY_EFAS26 + t00.thisYY_EFAS27 + t00.thisYY_EFAS48 + t00.thisYY_EFAS45 as thisYY_AMT, 
+  	t00.DAMBO_TYPE,
+  	-- 전전년 동월 담보가액
+  	t00.N2_EFAS37 + t00.N2_EFAS39 + t00.N2_EFAS22 + t00.N2_EFAS11 + t00.N2_EFAS12 + t00.N2_EFAS26 + t00.N2_EFAS27 + t00.N2_EFAS48 + t00.N2_EFAS45 as N2_AMT,
   	-- 전년 동월 담보가액
-  	t00.prevYY_EFAS37 + t00.prevYY_EFAS39 + t00.prevYY_EFAS22 + t00.prevYY_EFAS11 + t00.prevYY_EFAS12 + t00.prevYY_EFAS26 + t00.prevYY_EFAS27 + t00.prevYY_EFAS48 + t00.prevYY_EFAS45 as prevYY_AMT 
+  	t00.N1_EFAS37 + t00.N1_EFAS39 + t00.N1_EFAS22 + t00.N1_EFAS11 + t00.N1_EFAS12 + t00.N1_EFAS26 + t00.N1_EFAS27 + t00.N1_EFAS48 + t00.N1_EFAS45 as N1_AMT,
+  	-- 현월 담보가액
+  	t00.N0_EFAS37 + t00.N0_EFAS39 + t00.N0_EFAS22 + t00.N0_EFAS11 + t00.N0_EFAS12 + t00.N0_EFAS26 + t00.N0_EFAS27 + t00.N0_EFAS48 + t00.N0_EFAS45 as AMT_N0 	
+  	INTO RESULT_BIZDamboTrend_EFAS
 FROM 
   	(
     SELECT DISTINCT 
     	t0.EFAS, 
 	  	t0.DAMBO_TYPE, 
 	  	-- 자동차(37) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '37' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS37, 
-	  	SUM(CASE WHEN t0.EFAS = '37' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS37, 
+	  	SUM(CASE WHEN t0.EFAS = '37' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS37, 
+	  	SUM(CASE WHEN t0.EFAS = '37' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS37, 
+	  	SUM(CASE WHEN t0.EFAS = '37' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS37,
 	  	-- 조선(39) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '39' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS39, 
-	  	SUM(CASE WHEN t0.EFAS = '39' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS39,
+	  	SUM(CASE WHEN t0.EFAS = '39' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS39, 
+	  	SUM(CASE WHEN t0.EFAS = '39' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS39,
+	  	SUM(CASE WHEN t0.EFAS = '39' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS39,
 	  	-- 철강(22) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '22' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS22, 
-	  	SUM(CASE WHEN t0.EFAS = '22' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS22,
+	  	SUM(CASE WHEN t0.EFAS = '22' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS22, 
+	  	SUM(CASE WHEN t0.EFAS = '22' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS22,
+	  	SUM(CASE WHEN t0.EFAS = '22' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS22,
 	  	-- 정유(11) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '11' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS11, 
-	  	SUM(CASE WHEN t0.EFAS = '11' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS11,
+	  	SUM(CASE WHEN t0.EFAS = '11' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS11, 
+	  	SUM(CASE WHEN t0.EFAS = '11' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS11,
+	  	SUM(CASE WHEN t0.EFAS = '11' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS11,
 	  	-- 석유화학(12) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '12' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS12, 
-	  	SUM(CASE WHEN t0.EFAS = '12' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS12,
+	  	SUM(CASE WHEN t0.EFAS = '12' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS12, 
+	  	SUM(CASE WHEN t0.EFAS = '12' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS12,
+	  	SUM(CASE WHEN t0.EFAS = '12' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS12,
 	  	-- 반도체(26) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '26' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS26, 
-	  	SUM(CASE WHEN t0.EFAS = '26' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS26,
+	  	SUM(CASE WHEN t0.EFAS = '26' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS26, 
+	  	SUM(CASE WHEN t0.EFAS = '26' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS26,
+	  	SUM(CASE WHEN t0.EFAS = '26' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS26,
 	  	-- 디스플레이(27) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '27' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS27, 
-	  	SUM(CASE WHEN t0.EFAS = '27' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS27,
+	  	SUM(CASE WHEN t0.EFAS = '27' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS27, 
+	  	SUM(CASE WHEN t0.EFAS = '27' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS27,
+	  	SUM(CASE WHEN t0.EFAS = '27' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS27,
 	  	-- 해운(48) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '48' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS48, 
-	  	SUM(CASE WHEN t0.EFAS = '48' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS48,
+	  	SUM(CASE WHEN t0.EFAS = '48' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS48, 
+	  	SUM(CASE WHEN t0.EFAS = '48' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS48,
+	  	SUM(CASE WHEN t0.EFAS = '48' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS48,
 	  	-- 건설(45) 담보현황(현월, 전년동월)
-	  	SUM(CASE WHEN t0.EFAS = '45' AND t0.GG_YM = :inputGG_YM THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as thisYY_EFAS45, 
-	  	SUM(CASE WHEN t0.EFAS = '45' AND t0.GG_YM = :inputGG_YM - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as prevYY_EFAS45 
+	  	SUM(CASE WHEN t0.EFAS = '45' AND t0.GG_YM = ${inputGG_YM} THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N0_EFAS45, 
+	  	SUM(CASE WHEN t0.EFAS = '45' AND t0.GG_YM = ${inputGG_YM} - 100 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N1_EFAS45,
+	  	SUM(CASE WHEN t0.EFAS = '45' AND t0.GG_YM = ${inputGG_YM} - 200 THEN t0.DAMBO_AMT ELSE 0 END) OVER(PARTITION BY t0.DAMBO_TYPE, t0.EFAS) as N2_EFAS45
     FROM 
       	(
         SELECT DISTINCT 
@@ -123,10 +145,21 @@ FROM
         FROM 
             BASIC_BIZ_DAMBO t 
         WHERE 
-            CAST(t.GG_YM AS INTEGER) in (:inputGG_YM, :inputGG_YM - 100)	-- 당월 및 전년 동월 
+            CAST(t.GG_YM AS INTEGER) in (${inputGG_YM}, ${inputGG_YM} - 100, ${inputGG_YM} - 200)	-- 최근 3개년 
+            AND t.BRWR_NO_TP_CD = '3'	-- 법인만
+			AND NVL(t.EFAS, '') <> '55'	-- 금융보험업 제외
         ) t0
   	) t00
 WHERE 
 	t00.EFAS in ('37', '39', '22', '11', '12', '26', '27', '48', '45')
 ORDER BY 
   	To_number(t00.EFAS, '99'), DAMBO_TYPE;
+  	
+  
+
+  
+  
+-- 결과 조회
+SELECT * FROM RESULT_BIZDamboTrend ORDER BY GG_YM, DAMBO_TYPE;
+SELECT * FROM RESULT_BIZDamboTrend_UPKWON ORDER BY isBANK, DAMBO_TYPE;
+SELECT * FROM RESULT_BIZDamboTrend_EFAS ORDER BY TO_NUMBER(EFAS, '99'), DAMBO_TYPE;
