f221f8cf9750ea4486e5fe24bea0fb1366cbb873
Dongchan Oh <dynamic.oh@gmail.com>
2022-09-22 17:47:07 +0900

    Add files via upload

--- Enterprise_Finance_Analytic_System(EFAS)/신산업(newINDU)/신산업 기업대출/newINDU_BizLoan.sql
+++ Enterprise_Finance_Analytic_System(EFAS)/신산업(newINDU)/신산업 기업대출/newINDU_BizLoan.sql
@@ -6,22 +6,61 @@
 		
 /***********************************
  * 신산업별 금융권 익스포저
+ * RFP p.17 [그림1] 신산업별 금융권 익스포저
  * 활용 테이블 : BASIC_newBIZ_LOAN
  * 사용자 입력 : 조회기준년월(inputGG_YM)
  ***********************************/
+DROP TABLE IF EXISTS RESULT_newINDU_BIZLoan;
 SELECT DISTINCT
-	t.GG_YM,
-	SUM(t.BRNO_AMT) OVER(PARTITION BY t.GG_YM) as newINDU_AMT
-FROM  BASIC_newBIZ_LOAN t
-WHERE
-	CAST(t.GG_YM AS INTEGER) <= ${inputGG_YM};
+	t00.GG_YM,
+	ROUND(t00.TOT_newINDU_AMT, 0) as TOT_newINDU_AMT,
+	ROUND(SUM(t00.newINDU_AMT_1) OVER(PARTITION BY t00.GG_YM), 0) as '1 자율주행차',
+	ROUND(SUM(t00.newINDU_AMT_2) OVER(PARTITION BY t00.GG_YM), 0) as '2 IOT가전',
+	ROUND(SUM(t00.newINDU_AMT_3) OVER(PARTITION BY t00.GG_YM), 0) as '3 스마트 헬스케어',
+	ROUND(SUM(t00.newINDU_AMT_4) OVER(PARTITION BY t00.GG_YM), 0) as '4 바이오신약',
+	ROUND(SUM(t00.newINDU_AMT_5) OVER(PARTITION BY t00.GG_YM), 0) as '5 차세대반도체',
+	ROUND(SUM(t00.newINDU_AMT_6) OVER(PARTITION BY t00.GG_YM), 0) as '6 차세대디스플레이',
+	ROUND(SUM(t00.newINDU_AMT_7) OVER(PARTITION BY t00.GG_YM), 0) as '7 신재생에너지',
+	ROUND(SUM(t00.newINDU_AMT_8) OVER(PARTITION BY t00.GG_YM), 0) as '8 ESS',
+	ROUND(SUM(t00.newINDU_AMT_9) OVER(PARTITION BY t00.GG_YM), 0) as '9 스마트그리드',
+	ROUND(SUM(t00.newINDU_AMT_10) OVER(PARTITION BY t00.GG_YM), 0) as '10 전기차'
+	INTO RESULT_newINDU_BIZLoan
+FROM
+	(
+	SELECT 
+		t0.*,
+		DECODE(t0.newINDU_NM, '자율주행차', t0.newINDU_AMT, 0) as newINDU_AMT_1,
+		DECODE(t0.newINDU_NM, 'IOT 가전', t0.newINDU_AMT, 0) as newINDU_AMT_2,
+		DECODE(t0.newINDU_NM, '스마트 헬스케어', t0.newINDU_AMT, 0) as newINDU_AMT_3,
+		DECODE(t0.newINDU_NM, '바이오신약', t0.newINDU_AMT, 0) as newINDU_AMT_4,
+		DECODE(t0.newINDU_NM, '차세대반도체', t0.newINDU_AMT, 0) as newINDU_AMT_5,
+		DECODE(t0.newINDU_NM, '차세대디스플레이', t0.newINDU_AMT, 0) as newINDU_AMT_6,
+		DECODE(t0.newINDU_NM, '신재생에너지', t0.newINDU_AMT, 0) as newINDU_AMT_7,
+		DECODE(t0.newINDU_NM, 'ESS', t0.newINDU_AMT, 0) as newINDU_AMT_8,
+		DECODE(t0.newINDU_NM, '스마트그리드', t0.newINDU_AMT, 0) as newINDU_AMT_9,
+		DECODE(t0.newINDU_NM, '전기차', t0.newINDU_AMT, 0) as newINDU_AMT_10
+	FROM 	
+		(
+		SELECT DISTINCT
+			t.GG_YM,
+			t.newINDU,
+			t.newINDU_NM,
+			SUM(t.BRNO_AMT) OVER(PARTITION BY t.GG_YM, t.newINDU) as newINDU_AMT,
+			SUM(t.BRNO_AMT) OVER(PARTITION BY t.GG_YM) as TOT_newINDU_AMT
+		FROM  BASIC_newBIZ_LOAN t
+		WHERE
+			CAST(t.GG_YM AS INTEGER) <= ${inputGG_YM}
+			AND NVL(t.EFAS, '') <> '55'	-- 금융보험업 제외
+		) t0
+	) t00;	
 
 
 
-
+	
 
 /******************************************************
  * 신산업 세부현황 - 업종별 대출 잔액([표])
+ * RFP p.17 [표] 업종별 대출 잔액
  * 특수은행 : 01
  * 일반은행 : 03, 05, 07
  * 보험 : 11, 13, 15
@@ -31,28 +70,30 @@ WHERE
  * 저축은행 : 41
  * 기타 : 43, 46, 47, 75, 77, 83, 91, 94, 95, 97
  *******************************************************/
-SELECT DISTINCT 
+DROP TABLE IF EXISTS RESULT_newINDU_BIZLoan_Table;
+SELECT
 	t00.GG_YM,
-	t00.newINDU,
+	TO_NUMBER(t00.newINDU, '99') as "newINDU",
 	t00.newINDU_NM,
   	-- 일반은행 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '일반은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as GBANK, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '일반은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as GBANK, 
   	-- 특수은행 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '특수은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as SBANK, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '특수은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as SBANK, 
   	-- 보험 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '보험' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as BOHUM, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '보험' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as BOHUM, 
   	-- 금투 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '금투' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as KEUMTOO, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '금투' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as KEUMTOO, 
   	-- 여전 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '여전' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as YEOJUN, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '여전' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as YEOJUN, 
   	-- 상호금융 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '상호금융' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as SANGHO, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '상호금융' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as SANGHO, 
   	-- 저축은행 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '저축은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as JUCHOOK, 
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '저축은행' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as JUCHOOK, 
   	-- 기타 대출현황
-  	SUM(CASE WHEN t00.UPKWON = '기타' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU) as ETC,
+  	ROUND(SUM(CASE WHEN t00.UPKWON = '기타' THEN t00.newINDU_AMT ELSE 0 END) OVER(PARTITION BY t00.GG_YM, t00.newINDU), 0) as ETC,
   	t00.newINDU_TOT_AMT,
   	t00.newINDU_CNT
+  	INTO RESULT_newINDU_BIZLoan_Table
 FROM
   	(
     SELECT DISTINCT 
@@ -86,7 +127,11 @@ FROM
           	BASIC_newBIZ_LOAN t 
         WHERE
 			CAST(t.GG_YM AS INTEGER) <= ${inputGG_YM}
+			AND NVL(t.EFAS, '') <> '55'
       	) t0 
-  	) t00
-ORDER BY 
-	t00.GG_YM, t00.newINDU;
+  	) t00;
+	
+
+-- 결과 조회
+SELECT * FROM RESULT_newINDU_BIZLoan ORDER BY GG_YM;
+SELECT DISTINCT * FROM RESULT_newINDU_BIZLoan_Table ORDER BY GG_YM, newINDU;
